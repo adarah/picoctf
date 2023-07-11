421ba45c599ef5e9ea632d5a27ba4534e2dd172a
Lucas Harada <lucasyharada@gmail.com>
2023-07-11 22:11:26 +0000

    remove uneeded type annotation

--- whatsanetcat.py
+++ whatsanetcat.py
@@ -3,7 +3,7 @@ import pwn
 conn = pwn.remote('jupiter.challenges.picoctf.org', 25103)
 while True:
     try:
-        b: bytes = conn.recv()
+        b = conn.recv()
         print(b.decode(), end="")
     except EOFError:
         break
