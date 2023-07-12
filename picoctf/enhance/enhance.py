91b2903870071827bee4b2aeb9aa06b02b0b8ac0
Lucas Harada <lucasyharada@gmail.com>
2023-07-12 00:47:05 +0000

    solve enhance

---
+++ picoctf/enhance/enhance.py
@@ -0,0 +1,12 @@
+# https://play.picoctf.org/practice/challenge/265
+from bs4 import BeautifulSoup
+
+with open('./drawing.flag.svg') as f:
+    soup = BeautifulSoup(f, features="xml")
+    txt = soup.getText().splitlines()
+    for t in txt:
+        joined = t.replace(' ', '')
+        if not joined.startswith('picoCTF'):
+            continue
+        print(joined)
+        break
