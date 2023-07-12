56520e1b7c6e6214781b94ebdb7e89c31497aaa2
Lucas Harada <lucasyharada@gmail.com>
2023-07-12 00:35:55 +0000

    make script executable

--- picoctf/information/information.sh
+++ picoctf/information/information.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
-sudo apt update && sudo apt install exiftool
+# https://play.picoctf.org/practice/challenge/186
+# sudo apt update && sudo apt install exiftool
 exiftool cat.jpg | grep License | awk '{print $3}' | base64 -d
